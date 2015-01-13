581ff8b2e75f82bc767de96c4892f141a6243190
DaCHRIS <dachrls0@googlemail.com>
2015-01-13 13:23:15 +0100

    Update mkdata.sh

--- mkdata.sh
+++ mkdata.sh
@@ -8,7 +8,7 @@ jq -s '.[0] * .[1]' /var/www/alfred_158.json /var/www/alfred_159.json > /var/www
 # Node Daten 
 /etc/ffmap/bat2nodes.py -A -a /etc/ffmap/aliases.json -a /var/www/alfred_merged.json -o -d /var/www
 
-# fastd Daten 
+# fastd Daten (ANPASSEN WENN MEHRERE FASTD INSTANZEN AUF DEM GATEWAY, SIEHE README)
 /etc/fastd/status.pl /tmp/fastd.sock | jq . | sed -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/127\.0\.0\.1/ > /var/www/fastd.json
 
 
